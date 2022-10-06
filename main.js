1666878111071c5b4576191b2f472e72ca613f7f
name <yousefromeia@yahoo.com>
2022-10-06 17:19:36 +0200

    intial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+let car = document.getElementById("car");
+let carr = document.getElementById("carr");
+let carrr = document.getElementById("carrr");
+let carrrr = document.getElementById("carrrr");
+let containerr = document.querySelector(".containerr")
+
+car.onclick = function(){
+    containerr.style.backgroundImage = "url(./img/6.jpg)"
+
+}
+
+
+
+carr.onclick = function(){
+    containerr.style.backgroundImage = "url(./img/24.jpg)"
+
+}
+
+carrr.onclick = function(){
+    containerr.style.backgroundImage = "url(./img/3.jpg)"
+
+}
+
+carrrr.onclick = function(){
+    containerr.style.backgroundImage = "url(./img/2.jpg)"
+
+}
